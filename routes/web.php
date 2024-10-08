bfaecfb158118bd8426b40f377f0099b510a1487
Rizqi Nur Andi Putra <e31211961@student.polije.ac.id>
2024-10-08 17:51:21 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -24,26 +24,33 @@ Route::post('/login', [AuthController::class, 'login'])->name('auth-login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 
-//dashboard
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+// group middleware
+Route::middleware('isLogin')->group(function () {
+    //dashboard routes
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/users', [UserController::class, 'index'])->name('user.index');
-Route::post('/users', [UserController::class, 'store'])->name('user.store');
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+    //user routes
+    Route::get('/users', [UserController::class, 'index'])->name('user.index');
+    Route::post('/users', [UserController::class, 'store'])->name('user.store');
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('user.destroy');
 
-Route::get('/pasien', [PasienController::class, 'index'])->name('pasiens.index');
-Route::post('/pasien', [PasienController::class, 'store'])->name('pasiens.store');
-Route::put('/pasien/{nik}', [PasienController::class, 'update'])->name('pasiens.update');
-Route::delete('/pasien/{nik}', [PasienController::class, 'destroy'])->name('pasiens.destroy');
+    //pasiens routes
+    Route::get('/pasien', [PasienController::class, 'index'])->name('pasiens.index');
+    Route::post('/pasien', [PasienController::class, 'store'])->name('pasiens.store');
+    Route::put('/pasien/{nik}', [PasienController::class, 'update'])->name('pasiens.update');
+    Route::delete('/pasien/{nik}', [PasienController::class, 'destroy'])->name('pasiens.destroy');
 
-Route::get('/obat', [ObatController::class, 'index'])->name('obat.index');
-Route::post('/obat', [ObatController::class, 'store'])->name('obat.store');
-Route::put('/obat/{id}', [ObatController::class, 'update'])->name('obat.update');
-Route::delete('/obat/{id}', [ObatController::class, 'destroy'])->name('obat.destroy');
+    //obat routes
+    Route::get('/obat', [ObatController::class, 'index'])->name('obat.index');
+    Route::post('/obat', [ObatController::class, 'store'])->name('obat.store');
+    Route::put('/obat/{id}', [ObatController::class, 'update'])->name('obat.update');
+    Route::delete('/obat/{id}', [ObatController::class, 'destroy'])->name('obat.destroy');
 
-Route::get('/obatmasuk', [ObatMasukController::class, 'index'])->name('obatmasuk.index');
-Route::post('/obatmasuk', [ObatMasukController::class, 'store'])->name('obatmasuk.store');
-Route::put('/obatmasuk/{obatmasuk}', [ObatMasukController::class, 'update'])->name('obatmasuk.update');
-Route::delete('/obatmasuk/{obatmasuk}', [ObatMasukController::class, 'destroy'])->name('obatmasuk.destroy');
+    //obat masuk routes    
+    Route::get('/obatmasuk', [ObatMasukController::class, 'index'])->name('obatmasuk.index');
+    Route::post('/obatmasuk', [ObatMasukController::class, 'store'])->name('obatmasuk.store');
+    Route::put('/obatmasuk/{obatmasuk}', [ObatMasukController::class, 'update'])->name('obatmasuk.update');
+    Route::delete('/obatmasuk/{obatmasuk}', [ObatMasukController::class, 'destroy'])->name('obatmasuk.destroy');
 
+});
 
